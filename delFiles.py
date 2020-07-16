15551d9fff1d0da0988e9d59b20bbc9f827b3317
Vibhav <vibhavknirmal@gmail.com>
2020-07-16 23:27:15 +0530

    Update delFiles.py

--- delFiles.py
+++ delFiles.py
@@ -3,28 +3,29 @@ import glob
 
 os.chdir(input("Enter the Directory..\n"))
 
-print("Which type of files you want to delete?")
-print("Example Format:")
-print("txt / pdf / doc / jpg")
+print("Which type of files you want to delete?\n")
+print("Example Format:\n")
+print(" txt / pdf / doc / jpg / ini \n")
+print("(Do not include dot(.), only write file format)\n")
 theFormat = input("Enter the Format here: ")
 
 files = glob.glob('*.'+theFormat, recursive=False)
 print(files)
 
-#This part is still being updated
-#file = open("..\\DeletedFiles.txt", "w")
-#file.write(str(files))
-
 print("These Files will be Deleted, Do you wish to continue?")
-finalAns = input("Type Y if yes and N for no.")
+finalAns = input("Type Y to continue and N to abort.")
 
-if finalAns == "Y":
+if finalAns == "Y" or finalAns == "y":
     for i in range(len(files)):
         if os.path.isfile(files[i]):
             os.remove(files[i])
         else:
-            print("Error: %s file not found" % files[i])
-elif finalAns == "N":
+            print("Error: {} file not found".format(files[i]))
+    
+    fileN = open("..\\DeletedFiles.txt", "w")
+    fileN.write(str(files))
+
+elif finalAns == "N" or finalAns == "n":
     print("Aborting Operation..")
 else:
-    print("Enter Only Capital Y or Capital N..")
+    print("Invalid input.")
